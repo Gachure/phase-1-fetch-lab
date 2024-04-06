5eac37045b88c9d89c568aaa3262502ee76bcd81
victor <victor.gachure@student.moringaschool>
2024-04-06 15:52:45 +0300

    done lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -20,6 +20,7 @@ describe( "index.js", () => {
       expect( window.fetch, "A fetch to the API was not found" )
         .to.have.been.called.with( 'https://anapioficeandfire.com/api/books' );
     } )
+    
 
     it( "renders book titles into the DOM by passing a JSON object to renderBooks()", async () => {
       chai.spy.on( window, 'renderBooks' );
